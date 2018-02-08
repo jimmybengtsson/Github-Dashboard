b3c919147f8709a91c22da8ec725663d150687a6
jimmybengtsson <jb223pu@student.lnu.se>
2018-02-08 15:15:10 +0100

    Refactor code

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import './style/index.css';
+import App from './views/App';
+import registerServiceWorker from './components/registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
